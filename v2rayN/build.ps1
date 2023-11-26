a79031231b10a57c43afde652fadb7759c107c8a
canmeng12 <108603664+canmeng12@users.noreply.github.com>
2023-11-26 14:51:26 +0800

    xz

--- v2rayN/build.ps1
+++ v2rayN/build.ps1
@@ -32,15 +32,7 @@ New-Item -ItemType Directory -Name $OutputPath | Out-Null
 Push-Location $OutputPath
 New-Item -ItemType Directory -Name 'bin'  | Out-Null
 
-if ( -Not ( Test-Path '.\Other\release' ) ) {
-	.\Other\build.ps1
-	if ( -Not $? ) {
-		exit $lastExitCode
-	}
-}
-cp -Force '.\Other\release\*.bin' "$OutputPath\bin"
-cp -Force '.\Other\release\*.dll' "$OutputPath\bin"
-cp -Force '.\Other\release\*.exe' "$OutputPath\bin"
+
 
 if ( -Not ( Test-Path ".\v2rayN\bin\$Configuration" ) ) {
 	Write-Host
@@ -60,37 +52,37 @@ if ( -Not ( Test-Path ".\v2rayN\bin\$Configuration" ) ) {
 		'.\v2rayN\v2rayN.csproj'
 	if ( -Not $? ) { exit $lastExitCode }
 }
-cp -Force ".\v2rayN\binNetch\bin\$Configuration\v2rayN.exe" $OutputPath
+#cp -Force ".\v2rayN\bin\$Configuration\v2rayN.exe" $OutputPath
 
-if ( -Not ( Test-Path ".\Redirector\bin\$Configuration" ) ) {
-	Write-Host
-	Write-Host 'Building Redirector'
+# if ( -Not ( Test-Path ".\Redirector\bin\$Configuration" ) ) {
+# 	Write-Host
+# 	Write-Host 'Building Redirector'
 
-	msbuild `
-		-property:Configuration=$Configuration `
-		-property:Platform=x64 `
-		'.\Redirector\Redirector.vcxproj'
-	if ( -Not $? ) { exit $lastExitCode }
-}
-cp -Force ".\Redirector\bin\$Configuration\nfapi.dll"      "$OutputPath\bin"
-cp -Force ".\Redirector\bin\$Configuration\Redirector.bin" "$OutputPath\bin"
+# 	msbuild `
+# 		-property:Configuration=$Configuration `
+# 		-property:Platform=x64 `
+# 		'.\Redirector\Redirector.vcxproj'
+# 	if ( -Not $? ) { exit $lastExitCode }
+# }
+# cp -Force ".\Redirector\bin\$Configuration\nfapi.dll"      "$OutputPath\bin"
+# cp -Force ".\Redirector\bin\$Configuration\Redirector.bin" "$OutputPath\bin"
 
-if ( -Not ( Test-Path ".\RouteHelper\bin\$Configuration" ) ) {
-	Write-Host
-	Write-Host 'Building RouteHelper'
+# if ( -Not ( Test-Path ".\RouteHelper\bin\$Configuration" ) ) {
+# 	Write-Host
+# 	Write-Host 'Building RouteHelper'
 
-	msbuild `
-		-property:Configuration=$Configuration `
-		-property:Platform=x64 `
-		'.\RouteHelper\RouteHelper.vcxproj'
-	if ( -Not $? ) { exit $lastExitCode }
-}
-cp -Force ".\RouteHelper\bin\$Configuration\RouteHelper.bin" "$OutputPath\bin"
+# 	msbuild `
+# 		-property:Configuration=$Configuration `
+# 		-property:Platform=x64 `
+# 		'.\RouteHelper\RouteHelper.vcxproj'
+# 	if ( -Not $? ) { exit $lastExitCode }
+# }
+# cp -Force ".\RouteHelper\bin\$Configuration\RouteHelper.bin" "$OutputPath\bin"
 
-if ( $Configuration.Equals('Release') ) {
-	rm -Force "$OutputPath\*.pdb"
-	rm -Force "$OutputPath\*.xml"
-}
+# if ( $Configuration.Equals('Release') ) {
+# 	rm -Force "$OutputPath\*.pdb"
+# 	rm -Force "$OutputPath\*.xml"
+# }
 
 Pop-Location
 exit 0
