196a30659e9e248473b34ebb90c7c4904a685699
canmeng12 <108603664+canmeng12@users.noreply.github.com>
2023-11-28 00:08:22 +0800

    1

--- v2rayN/build.ps1
+++ v2rayN/build.ps1
@@ -1,11 +1,7 @@
-Write-Host 'Building'
-
-dotnet publish -c Release --self-contained false -o $env:Wap_Project_Directory
+dotnet publish -c Release --self-contained false -o $env:Wap_Project_Directory 
 
 #rm -Force .\$env:Wap_Project_Directory\*.pdb &&
 #rm -Force .\$env:Wap_Project_Directory\*.xml
 
-ls .\v2rayN\bin\v2rayN
-if ($LASTEXITCODE) { exit $LASTEXITCODE } 
+#if ($LASTEXITCODE) { exit $LASTEXITCODE } 
 
-Write-Host 'Build done'
