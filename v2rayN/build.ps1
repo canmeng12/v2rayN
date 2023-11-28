95a94e91f34fe004efdc20eb9b5bff9bbb4bc4fd
canmeng12 <108603664+canmeng12@users.noreply.github.com>
2023-11-28 22:50:14 +0800

    z

--- v2rayN/build.ps1
+++ v2rayN/build.ps1
@@ -1,8 +1,39 @@
-dotnet publish -c Release --self-contained false -o $env:Wap_Project_Directory 
+param (
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]
+	$OutputPath = '.\bin\v2rayN'
+)
 
-#rm -Force .\$env:Wap_Project_Directory\*.pdb &&
-#rm -Force .\$env:Wap_Project_Directory\*.xml
+Write-Host 'Building'
 
-#if ($LASTEXITCODE) { exit $LASTEXITCODE } 
+dotnet publish `
+	.\v2rayN\v2rayN.csproj `
+	-c Release `
+	--self-contained false `
+	-p:PublishReadyToRun=true `
+	-p:PublishSingleFile=true `
+	-o $OutputPath
 
-          $env:Solution_Name -c Release --self-contained=false -p:PublishReadyToRun=true -p:PublishSingleFile=true
+dotnet publish `
+	.\v2rayUpgrade\v2rayUpgrade.csproj `
+	-c Release `
+	--self-contained false `
+	-p:PublishReadyToRun=true `
+	-p:PublishSingleFile=true `
+	-o $OutputPath
+
+if ( -Not $? ) {
+	exit $lastExitCode
+	}
+
+if ( Test-Path -Path .\bin\v2rayN ) {
+    rm -Force "$OutputPath\*.pdb"
+    rm -Force "$OutputPath\*.xml"
+}
+
+Write-Host 'Build done'
+
+ls $OutputPath
+7z a  ..\v2rayN.zip $OutputPath
+exit 0
